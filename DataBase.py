83577263c7331b255d35564608416d1c03c8d3e2
Ali Khodabandehlou <akhodabandehlou93@gmail.com>
2024-03-16 10:32:57 +0330

    ADD: write_to_db fuction in DataBase.oy

--- DataBase.py
+++ DataBase.py
@@ -14,8 +14,33 @@ def read_from_db(class_name: str) -> dict:
 
     dbFilePath = os.path.join(os.getcwd(), DB_PATH, f'{class_name}.json')
     if os.path.exists(dbFilePath):
-        dbFile = open(dbFilePath)
+        dbFile = open(dbFilePath, 'r')
         data = json.load(dbFile)
         dbFile.close()
         return data
     return {}
+
+def write_to_db(class_name: str, new_data: dict, update_method: str) -> None:
+    """Writes new data, removes or updates old data to DataBase based on class_name and update_method
+    
+    Args:
+        update_method choices: add, remove, update
+    """
+    
+    old_data = read_from_db(class_name)
+    new_keys = new_data.keys()
+
+    if update_method == 'add' or update_method == 'update':
+        for key in new_keys:
+            old_data[key] = new_data[key]
+    elif update_method == 'remove':
+        for key in new_keys:
+            old_data.pop(key)
+    else:
+        pass
+
+    dbFilePath = os.path.join(os.getcwd(), DB_PATH, f'{class_name}.json')
+    if os.path.exists(dbFilePath):
+        json_object = json.dumps(old_data, indent=4)
+        with open(dbFilePath, 'r') as dbFile:
+            dbFile.write(json_object)
