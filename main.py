1f932700c74f9acf27e0fd50409fcbb5766583ef
Ali Khodabandehlou <akhodabandehlou93@gmail.com>
2024-03-26 11:31:20 +0330

    UPDATE: change in menu handling and printing

--- main.py
+++ main.py
@@ -2,38 +2,21 @@ import os
 
 from Classes import Bank, Card, Transaction, Owner
 
-options = {
-    '0': {
-        '1': [1, 'Banks'],
-        '2': [2, 'Cards'],
-        '3': [3, 'Transactions'],
-        '0': [0, 'go back']
-    },
-}
 stage = 0
 
 if __name__ == '__main__':
     while True:
         os.system('cls||clear')
-        if stage == 0:
-            print('Welcome to your personal wallet manager.')
-            print('select an option:')
-            for key, value in options[str(stage)].items():
+        for msg in stages[str(stage)]['messages']:
+            print(msg)
+        print('select an option:')
+        for key, value in stages[str(stage)]['options'].items():
                 print(f'{key}) {value[1]}')
-            print(f'\nq) exit')
-        elif stage == 1:
-            print('List of your Banks:')
-        elif stage == 2:
-            print('List of your Cards:')
-        elif stage == 3:
-            print('List of your Transactions:')
-        else:
-            stage = 0
 
         new_stage = input()
         if new_stage == 'q':
             break
         elif new_stage.isnumeric():
-            stage = options[stage][0]
+            stage = stages[stage][0]
         else:
             stage = 0
