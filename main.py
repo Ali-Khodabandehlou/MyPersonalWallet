bcf223b5f98a14d55777aabf4a61d46dd2e716c4
Ali Khodabandehlou <akhodabandehlou93@gmail.com>
2024-04-02 11:35:24 +0330

    update: fix menu handling

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Classes import Bank, Card, Transaction, Owner
 
 with open('bin/stages.json', 'r') as f:
     stages = json.load(f)
-stage = 0
+stage = '0'
 
 if __name__ == '__main__':
     while True:
@@ -15,11 +15,16 @@ if __name__ == '__main__':
         print('select an option:')
         for key, value in stages[str(stage)]['options'].items():
                 print(f'{key}) {value[1]}')
+        print('q for quit')
 
         new_stage = input()
         if new_stage == 'q':
             break
         elif new_stage.isnumeric():
-            stage = stages[stage][0]
+            if stage == '0' and new_stage == '0':
+                 stage = '0'
+                 continue
+            print(stages[str(stage)]['options'][new_stage][0])
+            stage = stages[str(stage)]['options'][new_stage][0]
         else:
-            stage = 0
+            stage = '0'
