1f8dd545c8e4cd3dbf8eb479980eeb2b4c558fa6
Ali Khodabandehlou <akhodabandehlou93@gmail.com>
2024-06-24 19:15:51 +0330

    UPDATE: add owner model and admin

--- apps/wallet/admin.py
+++ apps/wallet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Owner
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('show_name', 'first_name', 'last_name')
+    list_filter = ('first_name', 'last_name', 'nick_name')
+    search_fields = ('first_name', 'last_name', 'nick_name')
+    # prepopulated_fields = {'tags': ('title',)}
+
+admin.site.register(Owner, OwnerAdmin)
--- apps/wallet/models.py
+++ apps/wallet/models.py
@@ -3,17 +3,21 @@ from django.db import models
 from ..utils.models import Base
 
 
-# class Owner(Base):
-#     """Owner model"""
+class Owner(Base):
+    """Owner model"""
 
-#     first_name = models.CharField(max_length=20)
-#     last_name = models.CharField(max_length=20)
-#     nick_name = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    nick_name = models.CharField(max_length=20, null=True, blank=True)
 
-#     def __str__(self) -> str:
-#         if self.nick_name is None:
-#             return f'{self.first_name} {self.last_name}'
-#         return self.nick_name
+    @property
+    def show_name(self):
+        if self.nick_name is None:
+            return f'{self.first_name} {self.last_name}'
+        return self.nick_name
+
+    def __str__(self) -> str:
+        return self.show_name()
     
 
 # class Wallet(Base):
